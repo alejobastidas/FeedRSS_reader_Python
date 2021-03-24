1e7093f5cee37f26a093002f9432b1f0800abdd2
Alejo Bastidas <55358288+alejobastidas@users.noreply.github.com>
2021-03-24 13:21:55 -0500

    Add files via upload

---
+++ feed_reader/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'cd2d6e3270675f1228479f82b9c8625f'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from feed_reader import routes
